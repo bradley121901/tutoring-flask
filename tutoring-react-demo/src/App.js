8a4abf898cb3981270c0b48d3ef82b870a3622fe
bradley121901 <bradley121901@gmail.com>
2024-06-14 17:49:24 -0400

    initial react structure

---
+++ tutoring-react-demo/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>John Doe's Portfolio</h1>
+        <p>
+          Welcome to the site. This site will showcase the many interests and
+          projects that I have.{' '}
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
