48b09cb679d2968784bd6f737ba102f646a21c83
bradley121901 <bradley121901@gmail.com>
2024-06-21 18:31:05 -0400

    new styles

--- tutoring-react-demo/src/Home.js
+++ tutoring-react-demo/src/Home.js
@@ -4,11 +4,15 @@ function Home() {
   return (
     <div className="home-heading">
       <p className="title">John Doe's Portfolio Site</p>
-      <a href="/tutoring-react/blackjack">
-        #Blackjack Project OOP concept dive in JAVA
-      </a>
-      <br></br>
-      <a href="/tutoring-react/contact">#Contact now!</a>
+
+      <link alt="" href="%PUBLIC_URL%/homepage.png" />
+      <div className="homelinks">
+        <a href="/tutoring-react/blackjack">
+          #Blackjack Project OOP concept dive in JAVA
+        </a>
+        <br></br>
+        <a href="/tutoring-react/contact">#Contact now!</a>
+      </div>
     </div>
   );
 }
