f6c560c5ea523cb2f3090b4ad0079ec538f70573
bradley121901 <bradley121901@gmail.com>
2024-06-21 17:49:40 -0400

    testing

--- tutoring-react-demo/src/Home.js
+++ tutoring-react-demo/src/Home.js
@@ -3,8 +3,10 @@ import './Home.css';
 function Home() {
   return (
     <div className="home-heading">
-      <p>John Doe's Portfolio Site</p>
-      <a href="/blackjack">Blackjack Project OOP concept dive in JAVA</a>
+      <p className="title">John Doe's Portfolio Site</p>
+      <a href="/tutoring-react/blackjack">
+        Blackjack Project OOP concept dive in JAVA
+      </a>
     </div>
   );
 }
