aac74a56f7f6736d31c2896dd11d0b27bbd1056b
bradley121901 <bradley121901@gmail.com>
2024-06-21 18:12:02 -0400

    url fix

--- tutoring-react-demo/src/App.js
+++ tutoring-react-demo/src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 import React from 'react';
 import Blackjack from './Blackjack';
 import Home from './Home';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <Router>
       <div>
-        <Routes basename="/tutoring-react">
-          <Route path="/" element={<Home />} />
-          <Route path="/blackjack" element={<Blackjack />} />
+        <Routes>
+          <Route path="/tutoring-react" element={<Home />} />
+          <Route path="/tutoring-react/blackjack" element={<Blackjack />} />
         </Routes>
       </div>
     </Router>
